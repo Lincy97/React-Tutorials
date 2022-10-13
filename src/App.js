3ab8f131448c80d667e37b5def28b359aa2525d4
Lincy97 <linxy1997@gmail.com>
2022-10-13 10:49:55 +0800

    04_JSX1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import WithJSX from './components/WithJSX';
+import WithoutJSX from './components/WithoutJSX';
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
-      <Welcome />
+      <WithJSX/>
+      <WithoutJSX/>
+      {/* <Greet/>
+      <Welcome /> */}
     </div>
   );
 }
