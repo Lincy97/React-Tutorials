9d285e77a16eb68cf19fe0ed7f486e3da1295a54
Lincy97 <linxy1997@gmail.com>
2022-10-19 02:18:44 +0800

    06_State

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Hello from './components/05_Props';
-import Welcome from './components/05_Props1';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Hello name="Bruce" heroName="Batman">
-        <p>This is chilren props</p>
-      </Hello>
-      <Hello name="Clark" heroName="Superman">
-        <button>Action</button>
-      </Hello>
-      <Hello name="Diana" heroName="Wonder woman"/>
-
-      <Welcome name="Bruce" heroName="Batman"/>
-      <Welcome name="Clark" heroName="Superman"/>
-      <Welcome name="Diana" heroName="Wonder woman"/>
+      <Message></Message>
     </div>
   );
 }
--- src/components/Greet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-// function Greet() {
-//     return <h1>Hello Lincy</h1>
-// }
-
-const Greet = () => <h1>Hello Lincy Const</h1>;
-
-export default Greet;
