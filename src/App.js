22962e0fd758ca72662664010646511588fb4d13
Lincy97 <linxy1997@gmail.com>
2022-10-20 11:54:02 +0800

    07_setState_Increment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Message from './components/Message';
+import Count from './components/Count';
 
 function App() {
   return (
     <div className="App">
-      <Message></Message>
+      <Count/>
     </div>
   );
 }
