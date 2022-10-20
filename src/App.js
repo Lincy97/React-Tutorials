ff03941b413856256c5315e7706d4d07110fd745
Lincy97 <linxy1997@gmail.com>
2022-10-21 00:28:49 +0800

    07_setState_IncrementFive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Count from './components/Count';
+import Count5 from './components/Count5';
 
 function App() {
   return (
     <div className="App">
       <Count/>
+
+      <Count5/>
     </div>
   );
 }
