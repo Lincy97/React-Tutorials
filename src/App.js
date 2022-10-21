081c3d1df8288f7eab5955e8beb9f9e3a69f73cf
Lincy97 <linxy1997@gmail.com>
2022-10-22 00:44:59 +0800

    Methods as Props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick/>
-      <ClassClick/>
-      <EventBind/>
+      <ParentComponent />
     </div>
   );
 }
