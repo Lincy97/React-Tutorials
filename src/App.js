eaae877aec75104e30f94784a9c1c38fcc2b2374
Lincy97 <linxy1997@gmail.com>
2022-10-22 13:19:26 +0800

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
 
 
 function App() {
   return (
     <div className="App">
-      <ParentComponent />
+      <UserGreeting />
     </div>
   );
 }
