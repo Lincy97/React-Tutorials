7a257222c7af9bda84010740fd566f2256043ab7
Lincy97 <linxy1997@gmail.com>
2022-10-25 16:03:56 +0800

    Form Handling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyles.css';
-import styles from './appStyles.module.css';
+import Form from './components/Form';
 
 
 function App() {
   return (
     <div className="App">
-      <Stylesheet primary={true}/>
-      <Inline />
-      <h1 className="error">Error</h1>
-      <h1 className={styles.success}>Success</h1>
+      <Form />
     </div>
   );
 }
