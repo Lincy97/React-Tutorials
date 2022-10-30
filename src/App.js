e556776848670e1200e31c664132a9cb79a47747
Lincy97 <linxy1997@gmail.com>
2022-10-30 23:27:52 +0800

    Component Mounting And  Updating Lifecycle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
 
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <LifecycleA />
     </div>
   );
 }
