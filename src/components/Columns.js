dd7fa1620fae72b38160b0c7496338eeeb0daa47
Lincy97 <linxy1997@gmail.com>
2022-11-04 00:51:57 +0800

    Key attributes

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -1,12 +1,26 @@
 import React from 'react'
- 
+
 function Columns() {
+    const items = [
+        {
+            id: 1,
+            title: "Title A",
+            details: "Details A"
+        }
+    ]
     return(
         <React.Fragment>
-            <td>Name</td>
-            <td>Lincy</td>
+            {
+                items.map( item => (
+                    <React.Fragment key={item.id}>
+                        <td>Title</td>
+                        <td> {item.title} </td>
+                        <td> {item.details} </td>
+                    </React.Fragment>
+                ))
+            }
         </React.Fragment>
     )
 }
- 
+
 export default Columns
