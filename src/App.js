5f49cf13592fe3c33b37c3e97779a973196f9a59
Lincy97 <linxy1997@gmail.com>
2022-11-09 19:35:45 +0800

    Create ClickCounter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Hero from './components/Hero';
-import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
 
 function App() {
   return (
     <div className="App">
-      {/* <Hero heroName="Batman" />
-      <Hero heroName="Superman" />
-      <Hero heroName="Joker" /> */}
-
-      {/* <ErrorBoundary>
-        <Hero heroName="Batman" />
-        <Hero heroName="Superman" />
-        <Hero heroName="Joker" />
-      </ErrorBoundary> */}
-
-      <ErrorBoundary>
-        <Hero heroName="Batman" />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Superman" />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Hero heroName="Joker" />
-      </ErrorBoundary>
+      <ClickCounter />
     </div>
   );
 }
